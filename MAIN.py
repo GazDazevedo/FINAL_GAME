0f36044f007e86a6c744b38f22e781226fe7c27d
GazDazevedo <127146607+GazDazevedo@users.noreply.github.com>
2023-05-15 11:28:25 -0700

    fixed issue with change scree, added gold, court,s

--- MAIN.py
+++ MAIN.py
@@ -52,7 +52,9 @@ class Game:
         self.Turn = 1
         self.stability = 100
         self.courtopinion = 100
+        self.gold = 100
         self.badomens = 0
+        self.military = 0
         self.run()
         
         
@@ -63,7 +65,7 @@ class Game:
         text_rect = text_surface.get_rect()
         text_rect.midtop = (x,y)
         self.screen.blit(text_surface, text_rect)
-        print("DUMBASS\n")
+        
 
 
     def run(self):
@@ -92,6 +94,9 @@ class Game:
             self.screen.blit(King_image, King_image_rect)
             self.screen.blit(Textbox_image, Textbox_image_rect)
             self.screen.blit(Stats_image, Stats_image_rect)
+            self.draw_text("Stability: ", 40, BLACK, 1200, 500)
+            self.draw_text("Court: ", 40, BLACK, 1200, 600)
+            self.draw_text("Gold: ", 40, BLACK, 1200, 700)
             
             if self.numchoices >= 1:
                 Choices_image = pg.image.load(os.path.join(game_folder, 'Choices.jpg')).convert()
@@ -114,6 +119,24 @@ class Game:
             if self.Turn == 1:
                 self.draw_text("You are the ruler of the Great Kingdom of Bellarmine", 40, BLACK, 800, 200)
                 self.draw_text("Great! Tell me about it would you?", 30, BLACK, 650, 450)
+                self.draw_text("What am I doing?", 30, BLACK, 650, 600)
+                self.draw_text("I already know", 30, BLACK, 650, 800)
+            if self.Turn == 2:
+                self.draw_text("I'm no historian", 40, BLACK, 800, 200)
+                self.draw_text("You're fired", 30, BLACK, 650, 450)
+            if self.Turn == 3:
+                self.draw_text("Keep the kingdom alive, if a value is 0, you lose", 30, BLACK, 800, 200)
+                self.draw_text("You're not very helpful", 30, BLACK, 650, 450)
+            if self.Turn == 4: 
+                self.draw_text("Now it is time to make your military", 30, BLACK, 800, 200)
+                self.draw_text("Knights: High cost, great ability, good court", 30, BLACK, 650, 450)
+                self.draw_text("Peasants: No cost, not the best, bad court opinion", 30, BLACK, 650, 600)
+                self.draw_text("Mercenaries: Medium cost, decent, untrustorthy", 30, BLACK, 650, 800)
+            if self.Turn == 5:
+                self.draw_text("Now it is time to make your court", 30, BLACK, 800, 200)
+                self.draw_text("Jesters: Low cost, insane, easy to please", 30, BLACK, 650, 450)
+                self.draw_text("Nobles: Hight cost, competent, content", 30, BLACK, 650, 600)
+                self.draw_text("Democracy? Not in my kingdom", 30, BLACK, 650, 800)
 
                         
         else:
@@ -141,13 +164,67 @@ class Game:
                 if self.Turn == 1:
                     if (Choices_image_rect.collidepoint(self.mouse_coords) == True):
                         self.numchoices = 1
+                        self.mouse_coords = 0,0
                         self.Turn = 2
                     if (Choices2_image_rect.collidepoint(self.mouse_coords) == True):
                         self.numchoices = 1
+                        self.mouse_coords = 0,0
                         self.Turn = 3
                     if (Choices3_image_rect.collidepoint(self.mouse_coords) == True):
                         self.numchoices = 1
+                        self.mouse_coords = 0,0
                         self.Turn = 4
+                if self.Turn == 2:
+                    if (Choices_image_rect.collidepoint(self.mouse_coords) == True):
+                        self.numchoices = 3
+                        self.mouse_coords = 0,0
+                        self.Turn = 4
+                if self.Turn == 3:
+                    if (Choices_image_rect.collidepoint(self.mouse_coords) == True):
+                        self.numchoices = 3
+                        self.mouse_coords = 0,0
+                        self.Turn = 4
+                if self.Turn == 4:
+                    if (Choices_image_rect.collidepoint(self.mouse_coords) == True):
+                        self.numchoices = 3
+                        self.gold = self.gold - 10
+                        self.courtopinion = self.courtopinion + 5
+                        self.military = 3
+                        self.mouse_coords = 0,0
+                        self.Turn = 5
+                    if (Choices2_image_rect.collidepoint(self.mouse_coords) == True):
+                        self.numchoices = 3
+                        self.courtopinion = self.courtopinion - 10
+                        self.military = 1
+                        self.mouse_coords = 0,0
+                        self.Turn = 5
+                    if (Choices3_image_rect.collidepoint(self.mouse_coords) == True):
+                        self.numchoices = 3
+                        self.gold = self.gold - 5
+                        self.stability = self.stability - 10
+                        self.military = 2
+                        self.mouse_coords = 0,0
+                        self.Turn = 5
+                if self.Turn == 5:
+                    if (Choices_image_rect.collidepoint(self.mouse_coords) == True):
+                        self.numchoices = 3
+                        self.gold = self.gold - 5
+                        self.courtopinion = self.courtopinion + 20
+                        self.stability = self.stability - 25
+                        self.mouse_coords = 0,0
+                        self.Turn = 6
+                    if (Choices2_image_rect.collidepoint(self.mouse_coords) == True):
+                        self.numchoices = 3
+                        self.gold = self.gold - 20
+                        self.stability = self.stability + 20
+                        self.mouse_coords = 0,0
+                        self.Turn = 6
+                    if (Choices3_image_rect.collidepoint(self.mouse_coords) == True):
+                        self.numchoices = 3
+                        self.stability = self.stability - 50
+                        self.courtopinion = 1000
+                        self.mouse_coords = 0,0
+                        self.Turn = 6
     def update(self):
         self.draw()
 
